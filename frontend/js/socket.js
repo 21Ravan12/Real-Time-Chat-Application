9cf40c0422081f86357753add7aa4ee0b6d40c5e
Ravan Asgarov <asgarovravan@gmail.com>
2026-06-27 15:18:19 +0400

    Update socket connection URL for chat application

--- frontend/js/socket.js
+++ frontend/js/socket.js
@@ -6,7 +6,7 @@ function joinChat() {
   
   if (profileData && token) {
 
-    socket = io('https://real-time-chat-application-production-03a5.up.railway.app/', {
+    socket = io('https://real-time-chat-application-production-cb71.up.railway.app/', {
       auth: {
         token: token
       }
